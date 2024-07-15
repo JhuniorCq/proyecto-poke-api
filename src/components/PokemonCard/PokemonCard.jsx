13aca77c3f6d6b8b9f12e97e35bd6f3935feb011
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-07-14 23:43:45 -0500

    Agregando Loader

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -2,21 +2,18 @@ import { Link } from "react-router-dom";
 import { URL_POKEMON } from "../../constants";
 import { useGet } from "../../hooks/useGet";
 import { addZeros } from "./js";
-import styles from "./PokemonCard.module.css";
-import { useState } from "react";
 import { PokemonType } from "../PokemonType/PokemonType";
+import styles from "./PokemonCard.module.css";
 
 export const PokemonCard = ({ namePokemon }) => {
     
     const { responseGet: pokemonData, loadingGet, errorGet } = useGet(`${URL_POKEMON}/${namePokemon}`);
 
-    // responseGet salía como [] porque es su valor inicial hasta que la petición get a la PokeAPI me retorne otro valor (cuando retorna otro valor es cuando automáticametne deja de ser [] y ya es un ARRAY LLENO)
-    // Creo que con el pokemonData.length === 0 me ahorro el hecho de estar usando el Operador "?" cuando use las propieades de un Objeto
-    if(!pokemonData || pokemonData.length === 0 ) return;
+    if(loadingGet) return;
     
-    const imagePokemon = pokemonData?.sprites?.other["official-artwork"]?.front_default;
+    const imagePokemon = pokemonData.sprites.other["official-artwork"].front_default;
     const typesPokemon = pokemonData.types;
-    const idPokemon = pokemonData?.id;
+    const idPokemon = pokemonData.id;
     const numberPokemon = addZeros(idPokemon.toString());
 
     return (
